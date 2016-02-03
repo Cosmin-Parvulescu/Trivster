54cb1724f223e89b6349698ceb3ff683a2aaa7b7
Cosmin_Parvulescu <Cosmin.Parvulescu@gmail.com>
2016-02-03 23:14:30 +0200

    Cleaned the code a bit.

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/room', rooms.router);
+app.use('/room', rooms);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,17 +1,8 @@
 var express = require('express');
-
 var router = express.Router();
-var apiRouter = express.Router();
 
 router.get('/:room', function (req, res) {
     res.render('room', { room: req.params.room });
 });
 
-apiRouter.get('/', function(req, res) {
-    res.json({ message: 'List of games' })
-});
-
-module.exports = {
-    router: router,
-    apiRouter: apiRouter
-};
+module.exports = router;
